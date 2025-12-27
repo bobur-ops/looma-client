0e5c933b5962646ff962f64aeb2f6f3f308d35a0
Muxammadbobur Komiljonov <U0110@so.local>
2025-12-27 12:06:15 +0500

    feat: list, skeleton && empty state

--- src/features/notes/components/NoteList.tsx
+++ src/features/notes/components/NoteList.tsx
@@ -2,19 +2,33 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { useGetNotesQuery } from "../api/queries";
 import { useMemo } from "react";
 import { Spinner } from "@/components/ui/spinner";
-import { NoteListItem } from "./NoteListItem";
+import { NoteListItem, NoteListItemSkeleton } from "./NoteListItem";
+import Empty from "@/components/ui/empty";
 
 export const NoteList = () => {
-  const { data, isFetching } = useGetNotesQuery();
+  const { data, isFetching, isLoading } = useGetNotesQuery();
 
   const notes = useMemo(() => {
-    if (!data) return <div>No Data Yet</div>;
+    const list = data?.data || [];
 
-    return data.data.map((note) => <NoteListItem key={note.id} data={note} />);
+    if (isLoading) {
+      return Array.from({ length: 10 }).map((_, index) => (
+        <NoteListItemSkeleton key={index} />
+      ));
+    }
+
+    if (!list.length && !isLoading)
+      return (
+        <div className="p-5">
+          <Empty />
+        </div>
+      );
+
+    return list.map((note) => <NoteListItem key={note.id} data={note} />);
   }, [data]);
 
   return (
-    <div className="h-full relative pt-11">
+    <div className="h-full relative pt-12">
       <NoteListHeader isFetching={isFetching} />
       <ScrollArea className="flex-1 h-full">
         <section>{notes}</section>
@@ -32,7 +46,7 @@ const NoteListHeader = ({ isFetching }: NoteListHeaderProps) => {
     if (isFetching)
       return (
         <div className="flex gap-2 items-center">
-          <Spinner /> <span>Updating...</span>
+          <Spinner size={16} /> <span>Updating...</span>
         </div>
       );
 
@@ -40,8 +54,8 @@ const NoteListHeader = ({ isFetching }: NoteListHeaderProps) => {
   }, [isFetching]);
 
   return (
-    <div className="absolute left-0 top-0 right-0 border-b h-11 flex justify-center items-center">
-      <h2 className="font-medium">{headerText}</h2>
+    <div className="absolute left-0 top-0 right-0 border-b h-12 flex justify-center items-center">
+      <h2>{headerText}</h2>
     </div>
   );
 };
--- src/features/notes/components/NoteListItem.tsx
+++ src/features/notes/components/NoteListItem.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import type { NoteListItemInterface } from "../types";
+import { Skeleton } from "@/components/ui/skeleton";
 
 type Props = {
   data: NoteListItemInterface;
@@ -10,11 +11,19 @@ export const NoteListItem = ({ data }: Props) => {
     <div
       role="button"
       tabIndex={0}
-      className="select-none border-b p-2 hover:bg-muted active:bg-accent"
+      className="select-none border-b flex items-center px-2 hover:bg-muted active:bg-accent h-10"
     >
       <span className={cn({ "text-muted-foreground": !data.title })}>
-        {data.title || "New Note"}
+        {data.title || "No title"}
       </span>
+    </div>
+  );
+};
+
+export const NoteListItemSkeleton = () => {
+  return (
+    <div className="h-10 flex justify-center items-center p-2 border-b">
+      <Skeleton className="h-3 w-full" />
     </div>
   );
 };
